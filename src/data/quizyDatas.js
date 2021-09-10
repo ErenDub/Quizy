1b048ac3381d0219317d8e052b13ce6463fc82b2
ErenDub <61358096+ErenDub@users.noreply.github.com>
2021-09-10 12:32:11 +0400

    third

--- src/data/quizyDatas.js
+++ src/data/quizyDatas.js
@@ -49,8 +49,169 @@ export const QuizyData = [
         ],
       },
       {
-        id: 2,
+        id: 3,
         question: 'შეკითხვა 3',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 1,
+          },
+        ],
+      },
+      {
+        id: 4,
+        question: 'შეკითხვა 4',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 5,
+        question: 'შეკითხვა 5',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 1,
+          },
+        ],
+      },
+      {
+        id: 6,
+        question: 'შეკითხვა 6',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 7,
+        question: 'შეკითხვა 7',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 8,
+        question: 'შეკითხვა 8',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 9,
+        question: 'შეკითხვა 9',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 10,
+        question: 'შეკითხვა 10',
         answeared: true,
         answers: [
           {
@@ -125,6 +286,241 @@ export const QuizyData = [
       {
         id: 2,
         question: 'შეკითხვა 3',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    topic: 'ინგლისური',
+    questions: [
+      {
+        id: 1,
+        question: 'შეკითხვა 1',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 1,
+          },
+        ],
+      },
+      {
+        id: 2,
+        question: 'შეკითხვა 2',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 3,
+        question: 'შეკითხვა 3',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 1,
+          },
+        ],
+      },
+      {
+        id: 4,
+        question: 'შეკითხვა 4',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 5,
+        question: 'შეკითხვა 5',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 1,
+          },
+        ],
+      },
+      {
+        id: 6,
+        question: 'შეკითხვა 6',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 7,
+        question: 'შეკითხვა 7',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 8,
+        question: 'შეკითხვა 8',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 9,
+        question: 'შეკითხვა 9',
+        answeared: true,
+        answers: [
+          {
+            value: 'პასუხი 1',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 2',
+            correct: 0,
+          },
+          {
+            value: 'პასუხი 3',
+            correct: 1,
+          },
+          {
+            value: 'პასუხი 4',
+            correct: 0,
+          },
+        ],
+      },
+      {
+        id: 10,
+        question: 'შეკითხვა 10',
         answeared: true,
         answers: [
           {
